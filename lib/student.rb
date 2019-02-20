4661848d7f7512389fada77c0c97db960c413122
Jared Harbison <jared.harbison@gmail.com>
2019-02-20 18:13:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,10 @@ class Student
     @name = name 
     @grade = grade 
   end 
+  
+  def create_table
+    
+  end 
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
